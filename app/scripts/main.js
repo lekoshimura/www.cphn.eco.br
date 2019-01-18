3d0173a6dc706e0641d4a2f165ad389b8733feeb
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2019-01-18 08:16:33 -0200

    linted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   // https://wikiki.github.io/components/carousel/
   var carousels = bulmaCarousel.attach(); 
 });
