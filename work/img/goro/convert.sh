088035f0e7d00860e0f23c0ae1e024bd83881b31
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2019-01-18 06:18:45 -0200

    Imagem responsiva Goro Hashimoto

--- work/img/goro/convert.sh
+++ work/img/goro/convert.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
-SRC="$1"
 LOW=60
+MEDIUM=92
 HIGH=100
-convert $SRC.png -quality $LOW output/"$SRC"_low.jpg
-convert $SRC.png -quality $LOW output/"$SRC"_low.webp
-convert $SRC.png -quality $LOW -resize 25% output/"$SRC"_"$LOW"q_25pc.jpg
-convert $SRC.png -quality $LOW -resize 25% output/"$SRC"_"$LOW"q_25pc.webp
-convert $SRC.png -quality $HIGH output/"$SRC"_high.jpg
-convert $SRC.png -quality $HIGH output/"$SRC"_high.webp
-convert $SRC.png -quality $HIGH -resize 25% output/"$SRC"_"$HIGH"q_25pc.jpg
-convert $SRC.png -quality $HIGH -resize 25% output/"$SRC"_"$HIGH"q_25pc.webp
+
+# ref: https://medium.freecodecamp.org/a-guide-to-responsive-images-with-ready-to-use-templates-c400bd65c433
+
+# Bulma Breakpoints
+#     mobile | up to 768px (+) | => sm
+#     tablet | from 769px  (+) | => md
+#    desktop | from 1024px (+) | => lg
+# widescreen | from 1216px (-) |
+#     fullhd | from 1408px (-) |
+
+# Largura original: 2547 pixels
+# lg | 2x | 2547 | é o tamanho máximo (=original)
+# sm | 1x | 769  | Bulma mobile-tablet breakpoint
+# md | 1x | 1024 | Bulma tablet-desktop breakpoint
+# lg | 1x | 2547 | mantive tamanho original porque não é muito grande
+# sm | 2x | 1538 | sm 2x
+# md | 2x | 2048 | md 2x
+
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_2x.png
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_1x.png
+convert goro.png -quality $MEDIUM -resize 2048 output/goro-md_2x.png
+convert goro.png -quality $MEDIUM -resize 1024 output/goro-md_1x.png
+convert goro.png -quality $MEDIUM -resize 1538 output/goro-sm_2x.png
+convert goro.png -quality $MEDIUM -resize 769  output/goro-sm_1x.png
+
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_2x.jpg
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_1x.jpg
+convert goro.png -quality $MEDIUM -resize 2048 output/goro-md_2x.jpg
+convert goro.png -quality $MEDIUM -resize 1024 output/goro-md_1x.jpg
+convert goro.png -quality $MEDIUM -resize 1538 output/goro-sm_2x.jpg
+convert goro.png -quality $MEDIUM -resize 769  output/goro-sm_1x.jpg
+
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_2x.webp
+convert goro.png -quality $MEDIUM -resize 2547 output/goro-lg_1x.webp
+convert goro.png -quality $MEDIUM -resize 2048 output/goro-md_2x.webp
+convert goro.png -quality $MEDIUM -resize 1024 output/goro-md_1x.webp
+convert goro.png -quality $MEDIUM -resize 1538 output/goro-sm_2x.webp
+convert goro.png -quality $MEDIUM -resize 769  output/goro-sm_1x.webp
