2e03e8174f75af4c29bbba8e86f8d24686a2aa78
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2019-01-20 04:54:47 -0200

    Banner verde

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
+var webPsupport = (function () {
+  var webP = new Image();
+  webP.onload = WebP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+});
+
 document.addEventListener('DOMContentLoaded', function (event) {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
