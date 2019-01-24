0c9f2a0b70db6961fb435c3266c12c1db4fce647
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2019-01-23 22:59:26 -0200

    banner verde claro

--- work/img/banner/convert.sh
+++ work/img/banner/convert.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-LOW=60
-MEDIUM=92
+LOW=40
+MEDIUM=80
 HIGH=100
 
-# ref: https://medium.freecodecamp.org/a-guide-to-responsive-images-with-ready-to-use-templates-c400bd65c433
+# ref: 
+# https://medium.freecodecamp.org/a-guide-to-responsive-images-with-ready-to-use-templates-c400bd65c433
+# https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/automating-image-optimization/#what-is-mozjpeg
+#   - q=80 or lower is fine for web content
 
 # Bulma Breakpoints
 #     mobile | up to 768px (+) | => sm
@@ -20,6 +23,7 @@ HIGH=100
 # sm | 2x | 1538 | sm 2x
 # md | 2x | 2048 | md 2x
 
+# png
 convert banner.png -quality $HIGH -resize 2560 output/banner-lg_2x.png
 convert banner.png -quality $HIGH -resize 2560 output/banner-lg_1x.png
 convert banner.png -quality $HIGH -resize 2048 output/banner-md_2x.png
@@ -27,16 +31,18 @@ convert banner.png -quality $HIGH -resize 1024 output/banner-md_1x.png
 convert banner.png -quality $HIGH -resize 1538 output/banner-sm_2x.png
 convert banner.png -quality $HIGH -resize 769  output/banner-sm_1x.png
 
-convert banner.png -quality $HIGH -resize 2560 output/banner-lg_2x.jpg
-convert banner.png -quality $HIGH -resize 2560 output/banner-lg_1x.jpg
-convert banner.png -quality $HIGH -resize 2048 output/banner-md_2x.jpg
-convert banner.png -quality $HIGH -resize 1024 output/banner-md_1x.jpg
-convert banner.png -quality $HIGH -resize 1538 output/banner-sm_2x.jpg
-convert banner.png -quality $HIGH -resize 769  output/banner-sm_1x.jpg
-
+# webp
 convert banner.png -quality $HIGH -resize 2560 output/banner-lg_2x.webp
 convert banner.png -quality $HIGH -resize 2560 output/banner-lg_1x.webp
 convert banner.png -quality $HIGH -resize 2048 output/banner-md_2x.webp
 convert banner.png -quality $HIGH -resize 1024 output/banner-md_1x.webp
 convert banner.png -quality $HIGH -resize 1538 output/banner-sm_2x.webp
 convert banner.png -quality $HIGH -resize 769  output/banner-sm_1x.webp
+
+# mozpeg
+convert output/banner-lg_2x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-lg_2x.jpg
+convert output/banner-lg_1x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-lg_1x.jpg
+convert output/banner-md_2x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-md_2x.jpg
+convert output/banner-md_1x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-md_1x.jpg
+convert output/banner-sm_2x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-sm_2x.jpg
+convert output/banner-sm_1x.webp PNM:- | cjpeg -quality $MEDIUM -outfile output/banner-sm_1x.jpg
